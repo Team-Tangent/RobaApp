55e4d2296abe8bc230cd79c3dee996f36255c56a
Ryan McKenzie <mckenzie.aviation@gmail.com>
2020-01-02 21:53:19 -0600

    Added .net core project 2.2 framework. Added all 3 layers.

---
+++ Roba/Roba/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Roba
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+
+    }
+
+}

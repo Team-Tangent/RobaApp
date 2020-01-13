2f3253af11c1f7060d71895d171d7b317b601b7e
Nevin Herring <nevin.herring@gmail.com>
2020-01-13 08:01:45 -0600

    added item component and item model in dotnet

---
+++ Roba/Roba/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Roba
+{
+    public class Item
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public DateTime CreatedOnDate { get; set; }
+        public int Owner { get; set; }
+        public bool LentOut { get; set; }
+        public bool CanBeBorrowed { get; set; }
+        public DateTime BorrowedDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        //public enum ItemType { get; set; }
+    }
+}

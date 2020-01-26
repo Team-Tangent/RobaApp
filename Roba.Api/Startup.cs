4cbdac6978464efcee62354b95efa09177ec3c92
Ryan McKenzie <mckenzie.aviation@gmail.com>
2020-01-25 23:20:20 -0600

    Finished adding middleware for authentication, added identity dbcontext and factory, initiated migration and update.

--- Roba.Api/Startup.cs
+++ Roba.Api/Startup.cs
@@ -13,11 +13,28 @@ using Microsoft.EntityFrameworkCore;
 using Roba.Api.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Roba.Api.Auth;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Roba.Api.Filters;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Roba.SqlDbServices;
+using System.Text;
+using NSwag.Generation.Processors.Security;
+using NSwag;
+using NSwag.AspNetCore;
 
 namespace Roba.Api
 {
     public class Startup
     {
+        private const string SecretKey = "alksdueoiqklsd7d763hs"; //<-- NEW: make up a random key here
+        private readonly SymmetricSecurityKey _signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SecretKey));
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -35,6 +52,97 @@ namespace Roba.Api
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            var googleOptions = Configuration.GetSection(nameof(GoogleAuthSettings));
+            services.Configure<GoogleAuthSettings>(options =>
+            {
+                options.ClientId = googleOptions[nameof(GoogleAuthSettings.ClientId)];
+                options.ClientSecret = googleOptions[nameof(GoogleAuthSettings.ClientSecret)];
+            });
+
+            var microsoftOptions = Configuration.GetSection(nameof(MicrosoftAuthSettings));
+            services.Configure<MicrosoftAuthSettings>(options =>
+            {
+                options.ClientId = microsoftOptions[nameof(MicrosoftAuthSettings.ClientId)];
+                options.ClientSecret = microsoftOptions[nameof(MicrosoftAuthSettings.ClientSecret)];
+            });
+
+            services.AddSpaStaticFiles(config =>
+            {
+                config.RootPath = Configuration["SpaRoot"];
+            });
+
+            var jwtOptions = Configuration.GetSection(nameof(JwtIssuerOptions));
+            services.Configure<JwtIssuerOptions>(options =>
+            {
+                options.Issuer = jwtOptions[nameof(JwtIssuerOptions.Issuer)];
+                options.Audience = jwtOptions[nameof(JwtIssuerOptions.Audience)];
+                options.SigningCredentials = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256);
+            });
+            var tokenValidationPrms = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = jwtOptions[nameof(JwtIssuerOptions.Issuer)],
+                ValidateAudience = true,
+                ValidAudience = jwtOptions[nameof(JwtIssuerOptions.Audience)],
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = _signingKey,
+                RequireExpirationTime = false,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(configureOptions =>
+            {
+                configureOptions.ClaimsIssuer = jwtOptions[nameof(JwtIssuerOptions.Issuer)];
+                configureOptions.TokenValidationParameters = tokenValidationPrms;
+                configureOptions.SaveToken = true; // allows token access in controller
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ApiUser", policy => policy.RequireClaim(
+                    Constants.JwtClaimIdentifiers.Rol,
+                    Constants.JwtClaims.ApiAccess));
+            });
+
+            var identityBuilder = services.AddIdentityCore<RobaIdentityUser>(o =>
+            { // add any custom password rules here
+            });
+            identityBuilder = new IdentityBuilder(
+                identityBuilder.UserType,
+                typeof(IdentityRole<Guid>),
+                identityBuilder.Services);
+            identityBuilder.AddEntityFrameworkStores<RobaIdentityDbContext>();
+            identityBuilder.AddRoleValidator<RoleValidator<IdentityRole>>();
+            identityBuilder.AddRoleManager<RoleManager<IdentityRole<Guid>>>();
+            identityBuilder.AddSignInManager<SignInManager<RobaIdentityUser>>();
+            identityBuilder.AddDefaultTokenProviders();
+
+            services.AddOpenApiDocument(options =>
+            {
+                options.DocumentName = "v1";
+                options.Title = "Simple CRM";
+                options.Version = "1.0";
+                options.DocumentProcessors.Add(new SecurityDefinitionAppender("JWT token",
+                    new List<string>(), //no scope names to add
+                    new OpenApiSecurityScheme
+                    {
+                        In = OpenApiSecurityApiKeyLocation.Header,
+                        Name = "Authorization",
+                        Type = OpenApiSecuritySchemeType.ApiKey,
+                        Description = "Type into the textbox: 'Bearer {your_JWT_token}'. You can get a JWT from endpoints: '/auth/register' or '/auth/login'"
+                    }
+                ));
+                options.OperationProcessors.Add(
+                    new OperationSecurityScopeProcessor("JWT token"));
+            });
+
+            services.AddResponseCaching();
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
@@ -42,8 +150,36 @@ namespace Roba.Api
                 .AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(o =>
+            {
+                o.Filters.Add(typeof(GlobalExceptionFilter));
+            })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(options =>
+                {
+                    var settings = options.SerializerSettings;
+                    settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                    settings.Converters = new JsonConverter[]
+                    {
+                    new IsoDateTimeConverter(),
+                    new StringEnumConverter(true)
+                    };
+                });
+            services.AddDbContext<RobaIdentityDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
+
+            services.AddSingleton<IJwtFactory, JwtFactory>();
+           // services.AddScoped<>(); Add later
+            services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
+            services.AddScoped<IUrlHelper, UrlHelper>(factory =>
+            {
+                var context = factory.GetService<IActionContextAccessor>().ActionContext;
+                return new UrlHelper(context);
+            });
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -61,17 +197,46 @@ namespace Roba.Api
             }
 
             app.UseHttpsRedirection();
+            app.UseResponseCaching();
+
             app.UseStaticFiles();
+            app.UseSpaStaticFiles();
             app.UseCookiePolicy();
 
             app.UseAuthentication();
+            app.UseOpenApi();
+            app.UseSwaggerUi3(settings =>
+            {
+                var microsoftOptions = Configuration.GetSection(nameof(MicrosoftAuthSettings));
+                settings.OAuth2Client = new OAuth2ClientSettings
+                {
+                    ClientId = microsoftOptions[nameof(MicrosoftAuthSettings.ClientId)],
+                    ClientSecret = microsoftOptions[nameof(MicrosoftAuthSettings.ClientSecret)],
+                    AppName = "Simple CRM",
+                    Realm = "Nexul Academy"
+                };
+            });
 
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "identity",
+                    template: "Identity/{controller=Account}/{action=Register}/{id?}");
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            app.UseWhen(
+                context => !context.Request.Path.StartsWithSegments("/api"),
+                appBuilder => appBuilder.UseSpa(spa =>
+                {
+                    spa.Options.SourcePath = "../simple-crm-cli";
+                    if (env.IsDevelopment())
+                    {
+                        spa.UseAngularCliServer(npmScript: "start");
+                    }
+                }));
         }
     }
 }
---
+++ Roba.SqlDbServices/RobaIdentityDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Roba.SqlDbServices
+{
+    public class RobaIdentityDbContextFactory : IDesignTimeDbContextFactory<RobaIdentityDbContext>
+    {
+        public RobaIdentityDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<RobaIdentityDbContext>();
+            builder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Roba;Trusted_Connection=True;MultipleActiveResultSets=true",
+                optionsbuilder => optionsbuilder.MigrationsAssembly(typeof(RobaIdentityDbContext).GetTypeInfo().Assembly.GetName().Name));
+            return new RobaIdentityDbContext(builder.Options);
+        }
+    }
+}

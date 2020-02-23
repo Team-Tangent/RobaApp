567e3e3e12a4a4f5f7d19305afe43a758d5d89de
Ryan McKenzie <mckenzie.aviation@gmail.com>
2020-02-23 14:01:08 -0600

    update dev2

---
+++ Roba/Roba.SqlDbServices/SqlItemData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Roba.SqlDbServices
+{
+    public class SqlItemData : IItemData
+    {
+        public Item Get(int itemId)
+        {
+            throw new NotImplementedException();
+        }
+        public void Add(Item item)
+        {
+            throw new NotImplementedException();
+        }
+        public void Update(Item item)
+        {
+            throw new NotImplementedException();
+        }
+        public void Commit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

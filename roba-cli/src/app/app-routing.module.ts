967b11bea48d62f45980290091ae47c13f34f925
Cabs-Here <mckenzie.aviation@gmail.com>
2020-03-01 13:43:44 -0600

    added land routing

--- roba-cli/src/app/app-routing.module.ts
+++ roba-cli/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ItemModule } from './Item/item.module';
+import { LandComponent } from './land/land.component';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'app-dashboard',
-    pathMatch: 'full'
+    pathMatch: 'full',
+    children: [
+      {
+        path: 'land',
+        component: LandComponent,
+      },
   }
 ];
 

7b4c6cf7853c0a57630f42ffd0c2026792785895
Cabs-Here <mckenzie.aviation@gmail.com>
2020-03-11 17:30:12 -0500

    user controller and sqluserdata

---
+++ Roba.SqlDbServices/SqlUserData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roba.SqlDbServices
+{
+    public class SqlUserData : IUserData
+    {
+        private readonly CrmIdentityDbContext context;
+
+        public SqlUserData(CrmIdentityDbContext context)
+        {
+            this.context = context;
+        }
+
+        public CrmIdentityUser Get(Guid id)
+        {
+            return context.Users.FirstOrDefault(x => x.Id == id);
+        }
+
+        public CrmIdentityUser GetSingle(string userName)
+        {
+            return context.Users.FirstOrDefault(x => x.UserName == userName);
+        }
+    }
+}

2931bfebdfb028a9844db74876124a8778bb9db2
Ryan McKenzie <mckenzie.aviation@gmail.com>
2020-03-18 00:49:55 -0500

    added item and user repos and api controllers

---
+++ Roba.SqlDbServices/SqlItemData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Roba.SqlDbServices
+{
+    public class SqlItemData : IItemData
+    {
+        private RobaDbContext context;
+
+        public SqlItemData(RobaDbContext context)
+        {
+            this.context = context;
+        }
+        public void AddItem(Item item)
+        {
+            context.Add(item);
+            context.SaveChanges();
+        }
+
+
+        public void DeleteItem(Item item)
+        {
+            context.Remove(item);
+            context.SaveChanges();
+        }
+
+        public List<Item> GetAllItemsForUser(int UserId)
+        {
+            return context.Items.Where(x => x.Owner == UserId).ToList();
+        }
+
+        public Item GetItemById(int ItemId)
+        {
+            return context.Items.FirstOrDefault(x => x.ItemId == ItemId);
+        }
+
+        public void UpdateItem(Item item)
+        {
+            //Talk about how to handle updating. Track changes on front end, and just sav
+            throw new NotImplementedException();
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Roba.SqlDbServices/SqlUserData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Roba.SqlDbServices
+{
+    public class SqlUserData : IUserData
+    {
+        private readonly RobaIdentityDbContext context;
+        public SqlUserData (RobaIdentityDbContext context)
+        {
+            this.context = context;
+        }
+        public List<RobaIdentityUser> GetAllUsers()
+        {
+            return context.Users.ToList();
+        }
+
+        public RobaIdentityUser GetUserById(Guid UserId)
+        {
+            return context.Users.FirstOrDefault(x => x.Id == UserId);
+        }
+
+        public void UpdateUser(RobaIdentityUser user)
+        {
+            context.Update(user);
+        }
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+    }
+}

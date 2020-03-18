4699a0885dc4f1b39e14a8aa17dc3882a2ff4f59
Ryan McKenzie <mckenzie.aviation@gmail.com>
2020-03-18 01:01:26 -0500

    updated reference

--- Roba.SqlDbServices/RobaDbContextFactory.cs
+++ Roba.SqlDbServices/RobaDbContextFactory.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore.Design;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace Roba.SqlDbServices
@@ -12,7 +13,7 @@ namespace Roba.SqlDbServices
         {
             var builder = new DbContextOptionsBuilder<RobaDbContext>();
             builder.UseSqlServer("Server=tcp:robaio.database.windows.net,1433;Initial Catalog=RobaDB;Persist Security Info=False;User ID=Nexul2020;Password=Tangent20;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",
-                optionsBuilder => optionsBuilder.MigrationsAssembly(typeof(CrmDbContext).GetTypeInfo().Assembly.GetName().Name));
+                optionsBuilder => optionsBuilder.MigrationsAssembly(typeof(RobaDbContext).GetTypeInfo().Assembly.GetName().Name));
             return new RobaDbContext(builder.Options);
         }
     }

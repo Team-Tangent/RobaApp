5e2d64c85b71423e59d9cb69208175b75caf034a
Ryan McKenzie <ryan@webenology.com>
2020-03-29 13:06:13 -0500

    updated item class and did datatabase migration

--- Roba.SqlDbServices/Migrations/RobaDb/20200329180403_UpdateItemClass.Designer.cs
+++ Roba.SqlDbServices/Migrations/RobaDb/20200329180403_UpdateItemClass.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Roba.SqlDbServices;
 
 namespace Roba.SqlDbServices.Migrations.RobaDb
 {
     [DbContext(typeof(RobaDbContext))]
-    partial class RobaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200329180403_UpdateItemClass")]
+    partial class UpdateItemClass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Roba.SqlDbServices/Migrations/RobaDb/20200329180403_UpdateItemClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Roba.SqlDbServices.Migrations.RobaDb
+{
+    public partial class UpdateItemClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageFileContent",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ImageFileContent",
+                table: "Items",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}

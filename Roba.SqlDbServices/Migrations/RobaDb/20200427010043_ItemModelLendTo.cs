f1cef857f6cea15d6b09def3d3f2852340fc937c
Nevin Herring <nevin.herring@gmail.com>
2020-04-26 20:04:26 -0500

    migrated new itemModel

---
+++ Roba.SqlDbServices/Migrations/RobaDb/20200427010043_ItemModelLendTo.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Roba.SqlDbServices.Migrations.RobaDb
+{
+    public partial class ItemModelLendTo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Owner",
+                table: "Items",
+                newName: "UserId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LentTo",
+                table: "Items",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LentTo",
+                table: "Items");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Items",
+                newName: "Owner");
+        }
+    }
+}
